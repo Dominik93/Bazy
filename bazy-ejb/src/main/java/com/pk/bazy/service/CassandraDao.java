861c3805d2a9a340326da7ca40ebe41ad0971df1
rafcik <pasiut11@gmail.com>
2016-06-11 17:34:53 +0200

    cassandra driver + logowanie

--- bazy-ejb/src/main/java/com/pk/bazy/service/CassandraDao.java
+++ bazy-ejb/src/main/java/com/pk/bazy/service/CassandraDao.java
@@ -2,17 +2,51 @@ package com.pk.bazy.service;
 
 import java.util.List;
 
+import org.jboss.logging.Logger;
+import org.jboss.logging.Logger.Level;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.Statement;
 import com.pk.bazy.model.Follower;
 import com.pk.bazy.model.Friend;
 import com.pk.bazy.model.Tweet;
 import com.pk.bazy.model.User;
 
 public class CassandraDao implements Dao {
+	private static String contactPoint = "192.168.56.101";
+	private static String keyspace = "cassandra";
+
+	private Cluster cluster;// = Cluster.builder().addContactPoint(contactPoint).build();
+	private Session session;// = cluster.connect(keyspace);
+
+	public CassandraDao() {
+		cluster = Cluster.builder().addContactPoint(contactPoint).build();
+		session = cluster.connect(keyspace);
+	}
 
 	@Override
 	public User login(String login, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = new User();
+		try {
+			Statement stmt = new SimpleStatement(
+					"select username, password FROM users where username = '" + login + "';");
+
+			ResultSet results = session.execute(stmt);
+			for (Row row : results) {
+				String passwd = row.getString("password");
+
+				if (password.equals(passwd)) {
+					user = new User(row.getString("username"), row.getString("password"));
+				}
+			}
+		} catch (Exception ex) {
+			Logger.getLogger(Dao.class.getName()).log(Level.INFO, null, ex);
+		}
+		return user;
 	}
 
 	@Override
@@ -24,7 +58,7 @@ public class CassandraDao implements Dao {
 	@Override
 	public void generateRandomData() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -74,6 +108,4 @@ public class CassandraDao implements Dao {
 		return null;
 	}
 
-	
-	
 }
